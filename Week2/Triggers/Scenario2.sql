c222e3087d4a1ef1014e038508e6805977eac1c9
Sankalpa Pramanik <104688563+GoldRoger200@users.noreply.github.com>
2024-08-03 20:48:26 +0530

    Create Scenario2.sql

---
+++ Week2/Triggers/Scenario2.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TRIGGER LogTransaction
+AFTER INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    INSERT INTO AuditLog (
+        TransactionID, 
+        AccountID, 
+        TransactionDate, 
+        Amount, 
+        TransactionType, 
+        LogTime
+    )
+    VALUES (
+        :NEW.TransactionID, 
+        :NEW.AccountID, 
+        :NEW.TransactionDate, 
+        :NEW.Amount, 
+        :NEW.TransactionType, 
+        SYSDATE
+    );
+END;
+/
